f6b9dc1ea6b36e8c62a831ee31bdaf88027ae8f2
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-05-19 08:53:02 -0700

    National Bank of Memes

---
+++ nationalBankOfMemes.py
@@ -0,0 +1,16 @@
+import sys
+def newMemeber():
+	print('Welcome to the National Meme Bank of America, would you like to join? Y/N')
+	user = raw_input()
+	if(user == 'Y')
+		print('What will your account name be?')
+		account = raw_input()
+		print('Just to make sure, your account name is ' + account + ' Y/N')
+		user =raw_input()
+		if( user == 'Y')
+			
+			
+	else
+		return 'Thanks for your time!'
+		
+	
