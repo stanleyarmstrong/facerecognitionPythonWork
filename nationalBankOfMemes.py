c635ecca73295c4499ca95294e9aff4fc57201da
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-05-22 08:41:35 -0700

    Finished the new memeber method. Waiting to downlaod Python on this system so I can if it runs'

--- nationalBankOfMemes.py
+++ nationalBankOfMemes.py
@@ -1,16 +1,21 @@
 import sys
 def newMemeber():
-	print('Welcome to the National Meme Bank of America, would you like to join? Y/N')
-	user = raw_input()
-	if(user == 'Y')
+	print('Welcome to the National Meme Bank of United States, would you like to join? Y/N')
+	user = raw_input().lower()
+	if(user == 'y')
+		user = 'n'
 		print('What will your account name be?')
 		account = raw_input()
-		print('Just to make sure, your account name is ' + account + ' Y/N')
-		user =raw_input()
-		if( user == 'Y')
-			
-			
+		while(user != 'y')
+			print('Just to make sure, your account name is ' + account + ' Y/N')
+			user =raw_input().lower()
+		user = 'n'
+		print('Awesome, you will be getting a new T-shirt in the mail, but before that happens what would you like your password to be?')
+		password = raw_input()
+		while(user != 'y')
+			print('Just to clarify your password is ' + password + ' Y/N')
+			user = raw_input().lower()
+		print('Your account is fully set up!')
 	else
 		return 'Thanks for your time!'
-		
-	
+newMemeber()		
