d6020264f692aea9966fc107a32357df5fdecdf2
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-05-26 08:20:29 -0700

    Adding print statements to make it look better

--- nationalBankOfMemes.py
+++ nationalBankOfMemes.py
@@ -3,25 +3,35 @@ import sqlite3
 connection = sqlite3.connect('memeBase.db')
 
 def newMemeber():
-	user = input('Welcome to the National Meme Bank of the United States, would you like to join? Y/N').lower()
+	print('Welcome to the National Meme Bank of the United States, would you like to join? Y/N')
+	user = input().lower()
 	if(user == 'y'):
 		user = 'n'
-		account =input('What will your account name be?')
+		print(('What will your account name be?'
+		account =input()
 		while(user != 'y'):
-			user = input('Just to make sure, your account name is ' + account + ' Y/N').lower()
+			print('Just to make sure, your account name is ' + account + ' Y/N')
+			user = input().lower()
 			while(user == 'n'):
-				account = input('What would your new account name be?')
+				print('What would your new account name be?')
+				account = input()
 				break
-		user = 'n' 
-		password = input('Awesome, you will be getting a new T-shirt in the mail, but before that happens what would you like your password to be?')
+		user = 'n'
+		print('Awesome, you will be getting a new T-shirt, but before that happens what would you like your password to be?')
+		password = input()
 		while(user != 'y'):
-			user = input('Just to clarify your password is ' + password + ' Y/N').lower()
+			print('Just to clarify your password is ' + password + ' Y/N')
+			user = input().lower()
 			while(user == 'n'):
-				password = input('What would you like your password to be?')
+				print('What would you like your password to be?')
+				password = input()
 				break
 		print('Your account is fully set up!')
 		return account + ' ' + password
 	else:
 		return 'Thanks for your time!'
 
-
+def deposit():
+	print('How memes would you like to deposit today?')
+	user = input()
+	
