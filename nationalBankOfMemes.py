8a8f231958be86d5043232f656c1c7febe9bd935
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2017-05-26 08:24:43 -0700

    Found the error

--- nationalBankOfMemes.py
+++ nationalBankOfMemes.py
@@ -7,7 +7,7 @@ def newMemeber():
 	user = input().lower()
 	if(user == 'y'):
 		user = 'n'
-		print('What will your account name be?'
+		print('What will your account name be?')
 		account = input()
 		while(user != 'y'):
 			print('Just to make sure, your account name is ' + account + ' Y/N')
